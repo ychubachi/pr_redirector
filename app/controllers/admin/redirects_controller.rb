058b8dd4f7487444760242fb98076577209d37db
Yoshihide Chubachi <yoshi@chubachi.net>
2012-06-16 03:50:01 +0900

    make redirect id shown

--- app/controllers/admin/redirects_controller.rb
+++ app/controllers/admin/redirects_controller.rb
@@ -3,7 +3,7 @@ module Admin
   class RedirectsController < ApplicationController
     active_scaffold :redirect do |conf|
       conf.label = "リダイレクト一覧"
-      conf.columns = [:medium, :target, :default_redirect]
+      conf.columns = [:id, :medium, :target, :default_redirect]
     end
   end
 end

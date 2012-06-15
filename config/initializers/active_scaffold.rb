b7cb2e867c4416d20225954aa2f359b42e584b87
Yoshihide Chubachi <yoshi@chubachi.net>
2012-06-16 01:26:16 +0900

    added links to the top menu

--- config/initializers/active_scaffold.rb
+++ config/initializers/active_scaffold.rb
@@ -1,4 +1,9 @@
 # config/initializers/active_scaffold.rb
 ActiveScaffold.set_defaults do |config| 
   config.ignore_columns.add [:created_at, :updated_at, :lock_version]
+  config.action_links.add 'index', controller: 'referrers', label: 'Referrers', page: true
+  config.action_links.add 'index', controller: 'users', label: 'Users', page: true
+  config.action_links.add 'index', controller: 'redirects', label: 'Redirects', page: true
+  config.action_links.add 'index', controller: 'targets', label: 'Targets', page: true
+  config.action_links.add 'index', controller: 'media', label: 'Media', page: true
 end

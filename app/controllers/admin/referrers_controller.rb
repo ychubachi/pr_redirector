a4cfea71315eec84a9d4f2c10390d2bcfcff3a7f
Yoshihide Chubachi <yoshi@chubachi.net>
2012-06-16 03:41:38 +0900

    made referrers title shown

--- app/controllers/admin/referrers_controller.rb
+++ app/controllers/admin/referrers_controller.rb
@@ -3,7 +3,7 @@ module Admin
   class ReferrersController < ApplicationController
     active_scaffold :referrer do |conf|
       conf.label = "参照元一覧"
-      conf.columns = [:created_at, :user, :redirect]
+      conf.columns = [:created_at, :user, :title, :redirect]
     end
   end
 end

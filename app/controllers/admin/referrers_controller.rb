3818b19800d92a8e4beef561c3fc53d3903e890c
Yoshihide Chubachi <yoshi@chubachi.net>
2012-06-17 23:22:12 +0900

    show user is admin

--- app/controllers/admin/referrers_controller.rb
+++ app/controllers/admin/referrers_controller.rb
@@ -3,7 +3,7 @@ module Admin
   class ReferrersController < ApplicationController
     active_scaffold :referrer do |conf|
       conf.label = "参照元一覧"
-      conf.columns = [:created_at, :user, :title, :redirect]
+      conf.columns = [:created_at, :user, :user_admin, :redirect]
     end
   end
 end
--- app/models/referrer.rb
+++ app/models/referrer.rb
@@ -2,4 +2,8 @@ class Referrer < ActiveRecord::Base
   attr_accessible :title
   belongs_to :user
   belongs_to :redirect
+
+  def user_admin
+    return user.admin
+  end
 end

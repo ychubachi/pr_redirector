ceb98127fa2fb187251954eed377fc7b10f45f4b
Yoshihide Chubachi <yoshi@chubachi.net>
2012-06-20 21:02:20 +0900

    created_at

--- app/controllers/admin/referrers_controller.rb
+++ app/controllers/admin/referrers_controller.rb
@@ -3,7 +3,7 @@ module Admin
   class ReferrersController < ApplicationController
     active_scaffold :referrer do |conf|
       conf.label = "参照元一覧"
-      # conf.list.columns = [:created_at, :title, :user, :user_admin, :redirect]
+      conf.list.columns = [:created_at, :title, :user, :user_admin, :remote_addr, :redirect]
       conf.list.sorting = {created_at: 'DESC'}
       conf.actions.exclude :create
       conf.actions.exclude :update
